2b6fcd7bf94a9b2a842ce8154934ffc315249b9c
lsamano <leizl.samano@gmail.com>
2019-03-19 16:52:09 -0400

    wip

---
+++ app/models/team_request.rb
@@ -0,0 +1,6 @@
+class TeamRequest < ApplicationRecord
+  belongs_to :user
+  belongs_to :team
+
+  validates_uniqueness_of :team_id, scope: :user_id
+end
---
+++ db/migrate/20190319203247_create_team_requests.rb
@@ -0,0 +1,9 @@
+class CreateTeamRequests < ActiveRecord::Migration[5.2]
+  def change
+    create_table :team_requests do |t|
+      t.integer :team_id
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
