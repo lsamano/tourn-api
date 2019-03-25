d1186cc1e2c85fad2d3c8bbb362235f870573ef6
lsamano <leizl.samano@gmail.com>
2019-03-25 17:36:28 -0400

    fixed Tourn create action

--- app/controllers/api/v1/tournaments_controller.rb
+++ app/controllers/api/v1/tournaments_controller.rb
@@ -1,36 +1,6 @@
 class Api::V1::TournamentsController < ApplicationController
   skip_before_action :authorized, only: [:index]
 
-  def bracket
-    # CREATE BRACKET
-    # Store entrant-teams into new array
-    teams_array = @tournament.teams.clone
-
-    # Create Round 1
-    round1 = Round.create(number:1, notes:"", best_of:7, tournament:@tournament)
-      # Create 4 GameSets
-    4.times do |x|
-      game_set = GameSet.create(round:round1, notes:"", team1_id: teams_array.delete_at(rand(teams_array.length)), team2_id: teams_array.delete_at(rand(teams_array.length)))
-      # Create 7 matches each?
-      round1.best_of.times do |x|
-        Match.create(game_set: game_set, notes:"")
-      end
-    end
-
-    # Create Round 2
-    round1 = Round.create(number:1, notes:"", best_of:7, tournament:@tournament)
-      # Create 4 GameSets
-    4.times do |x|
-      game_set = GameSet.create(round:round1, notes:"", team1_id: teams_array.delete_at(rand(teams_array.length)), team2_id: teams_array.delete_at(rand(teams_array.length)))
-      # Create 7 matches each?
-      round1.best_of.times do |x|
-        Match.create(game_set: game_set, notes:"")
-      end
-    end
-
-
-  end # End of bracket action
-
   def index
     tournaments = Tournament.all.sort_by { |tourn| tourn[:created_at] }.reverse
     tournaments = tournaments.map {|tourn| TournamentSerializer.new(tourn)}
@@ -43,7 +13,7 @@ class Api::V1::TournamentsController < ApplicationController
       # Add Host to Admins List
       Admin.create(user: current_user, tournament: @tournament)
       # Return Tournament and User/Host
-      render json: { tournament: @tournament, user: UserSerializer.new(current_user) }, status: :created
+      render json: { tournament: TournamentSerializer.new(@tournament), user: UserSerializer.new(current_user) }, status: :created
     else
       render json: { error: 'failed to create tournament' }, status: :not_acceptable
     end
