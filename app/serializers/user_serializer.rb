c0e3518706b0b09308569802602144e2bda9a685
lsamano <leizl.samano@gmail.com>
2019-03-26 13:42:29 -0400

    user serializer now includes created_at

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :avatar, :bio
+  attributes :id, :username, :avatar, :bio, :created_at
   has_many :teams, serializer: UserTeamSerializer
   # has_many :tournaments
   has_many :hosted_tourns, serializer: TeamTournamentSerializer
