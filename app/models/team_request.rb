de9b62475a0d1e43fae3e2971885a8bd25ffc1c6
Leizl Samano <leizl.samano@gmail.com>
2019-11-21 17:37:09 -0500

    note added on team request

--- app/models/team_request.rb
+++ app/models/team_request.rb
@@ -3,4 +3,6 @@ class TeamRequest < ApplicationRecord
   belongs_to :team
 
   validates_uniqueness_of :team_id, scope: :user_id
+
+  # Need validation for verifying that you're not already on the team
 end
